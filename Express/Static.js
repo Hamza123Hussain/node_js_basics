01f90d52390ec87dab161a517131cac73f8b8619
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-18 03:44:39 +0500

    Posting and Getting data from Mongo

--- Express/Static.js
+++ Express/Static.js
@@ -17,6 +17,7 @@ import Router from './Routes/Params_Query.js'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import express from 'express'
+import ProductModel from '../Model/Product.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -32,6 +33,31 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+//MONGODB CRUD BELOW
+//--------------------------------------------------------------------------------------------------
+app.post('/api/CreateProduct', async (req, res) => {
+  // adding data in db
+  try {
+    const product = req.body // giving data from body
+    const createdProduct = await ProductModel.create(product) //adding data to db
+    res.status(201).json(createdProduct) // printing created data
+  } catch (error) {
+    console.error('Error creating product:', error)
+    res.status(500).send('ERROR')
+  }
+})
+
+app.get('/api/Product', async (req, res) => {
+  // adding data in db
+  try {
+    const products = await ProductModel.find() //adding data to db
+    res.status(201).json(products) // printing created data
+  } catch (error) {
+    console.error('Error creating product:', error)
+    res.status(500).send('ERROR')
+  }
+})
+//----------------------------------------------------------------------------------------------------
 app.listen(8000, () => {
   console.log('PORT RUNNING')
 })
